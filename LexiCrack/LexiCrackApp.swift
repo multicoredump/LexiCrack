a861b72464d788afd448c1f3df9b83f3d1b7db82
Radhika K <multicoredump@gmail.com>
2026-02-15 19:07:05 -0800

    Initial Commit

---
+++ LexiCrack/LexiCrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexiCrackApp.swift
+//  LexiCrack
+//
+//  Created by Radhika Karandikar on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct LexiCrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
